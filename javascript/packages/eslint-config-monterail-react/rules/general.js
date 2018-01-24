1816eb938a70b7a3768a1c694984ec76c6028fbd
Belir <patrykpeas@gmail.com>
2018-01-24 16:23:55 +0100

    [ESLINT] Configure eslint-config-monterail-react for react plugin

--- javascript/packages/eslint-config-monterail-react/rules/general.js
+++ javascript/packages/eslint-config-monterail-react/rules/general.js
@@ -1,138 +1,169 @@
 module.exports = {
   rules: {
     // Enforces consistent naming for boolean props
-    'react/boolean-prop-naming': ,
+    'react/boolean-prop-naming': 'error',
 
     // Forbid "button" element without an explicit "type" attribute
-    'react/button-has-type': ,
+    'react/button-has-type': 'error',
 
     // Prevent extraneous defaultProps on components
-    'react/default-props-match-prop-types': ,
+    'react/default-props-match-prop-types': 'error',
 
     // Rule enforces consistent usage of destructuring assignment in component
-    'react/destructuring-assignment': ,
+    'react/destructuring-assignment': ['error', 'always'],
 
     // Prevent missing displayName in a React component definition
-    'react/display-name': ,
+    'react/display-name': 'off',
 
     // Forbid certain props on Components
-    'react/forbid-component-props': ,
+    'react/forbid-component-props': 'off',
 
     // Forbid certain props on DOM Nodes
-    'react/forbid-dom-props': ,
+    'react/forbid-dom-props': 'off',
 
     // Forbid certain elements
-    'react/forbid-elements': ,
+    'react/forbid-elements': 'off',
 
     // Forbid certain propTypes
-    'react/forbid-prop-types': ,
+    'react/forbid-prop-types': ['error', {
+      forbid: [
+        'any',
+        'array',
+        'object'
+      ],
+    }],
 
     // Forbid foreign propTypes
-    'react/forbid-foreign-prop-types': ,
+    'react/forbid-foreign-prop-types': 'off',
 
     // Prevent using this.state inside this.setState
-    'react/no-access-state-in-setstate': ,
+    'react/no-access-state-in-setstate': 'error',
 
     // Prevent using Array index in key props
-    'react/no-array-index-key': ,
+    'react/no-array-index-key': 'error',
 
     // Prevent passing children as props
-    'react/no-children-prop': ,
+    'react/no-children-prop': 'error',
 
     // Prevent usage of dangerous JSX properties
-    'react/no-danger': ,
+    'react/no-danger': 'error',
 
     // Prevent problem with children and props.dangerouslySetInnerHTML
-    'react/no-danger-with-children': ,
+    'react/no-danger-with-children': 'error',
 
     // Prevent usage of deprecated methods
-    'react/no-deprecated': ,
+    'react/no-deprecated': 'error',
 
     // Prevent usage of setState in componentDidMount
-    'react/no-did-mount-set-state': ,
+    'react/no-did-mount-set-state': 'error',
 
     // Prevent usage of setState in componentDidUpdate
-    'react/no-did-update-set-state': ,
+    'react/no-did-update-set-state': 'error',
 
     // Prevent direct mutation of this.state
-    'react/no-direct-mutation-state': ,
+    'react/no-direct-mutation-state': 'error',
 
     // Prevent usage of findDOMNode
-    'react/no-find-dom-node': ,
+    'react/no-find-dom-node': 'error',
 
     // Prevent usage of isMounted
-    'react/no-is-mounted': ,
+    'react/no-is-mounted': 'error',
 
     // Prevent multiple component definition per file
-    'react/no-multi-comp': ,
+    'react/no-multi-comp': ['error', {
+      ignoreStateless: true
+    }],
 
     // Prevent usage of shouldComponentUpdate when extending React.PureComponent
-    'react/no-redundant-should-component-update': ,
+    'react/no-redundant-should-component-update': 'error',
 
     // Prevent usage of the return value of React.render
-    'react/no-render-return-value': ,
+    'react/no-render-return-value': 'error',
 
     // Prevent usage of setState
-    'react/no-set-state': ,
+    'react/no-set-state': 'off',
 
     // Prevent common casing typos
-    'react/no-typos': ,
+    'react/no-typos': 'error',
 
     // Prevent using string references in ref attribute.
-    'react/no-string-refs': ,
+    'react/no-string-refs': 'error',
 
     // Prevent using this in stateless functional components
-    'react/no-this-in-sfc': ,
+    'react/no-this-in-sfc': 'error',
 
     // Prevent invalid characters from appearing in markup
-    'react/no-unescaped-entities': ,
+    'react/no-unescaped-entities': 'error',
 
     // Prevent usage of unknown DOM property(fixable)
-    'react/no-unknown-property': ,
+    'react/no-unknown-property': 'error',
 
     // Prevent definitions of unused prop types
-    'react/no-unused-prop-types': ,
+    'react/no-unused-prop-types': ['error', {
+      customValidators: [],
+      skipShapeProps: true,
+    }],
 
     // Prevent definitions of unused state properties
-    'react/no-unused-state': ,
+    'react/no-unused-state': 'error',
 
     // Prevent usage of setState in componentWillUpdate
-    'react/no-will-update-set-state': ,
+    'react/no-will-update-set-state': 'error',
 
     // Enforce ES5 or ES6 class for React Components
-    'react/prefer-es6-class': ,
+    'react/prefer-es6-class': 'error',
 
     // Enforce stateless React Components to be written as a pure function
-    'react/prefer-stateless-function': ,
+    'react/prefer-stateless-function': ['error', {
+      ignorePureComponents: true,
+    }],
 
     // Prevent missing props validation in a React component definition
-    'react/prop-types': ,
+    'react/prop-types': ['error', {
+      ignore: [],
+      customValidators: [],
+      skipUndeclared: false
+    }],
 
     // Prevent missing React when using JSX
-    'react/react-in-jsx-scope': ,
+    'react/react-in-jsx-scope': 'error',
 
     // Enforce a defaultProps definition for every prop that is not a required prop
-    'react/require-default-props': ,
+    'react/require-default-props': 'error',
 
     // Enforce React components to have a shouldComponentUpdate method
-    'react/require-optimization': ,
+    'react/require-optimization': 'off',
 
     // Enforce ES5 or ES6 class for returning value in render function
-    'react/require-render-return': ,
+    'react/require-render-return': 'error',
 
     // Prevent extra closing tags for components without children(fixable)
-    'react/self-closing-comp': ,
+    'react/self-closing-comp': 'error',
 
     // Enforce component methods order(fixable)
-    'react/sort-comp': ,
+    'react/sort-comp': [
+      'error',
+      {
+        order: [
+          'type-annotations',
+          'static-methods',
+          'lifecycle',
+          '/^(on|handle).+$/',
+          '/^(get|set)(?!(InitialState$|DefaultProps$|ChildContext$)).+$/',
+          'everything-else',
+          '/^render.+$/',
+          'render'
+        ]
+      }
+    ],
 
     // Enforce propTypes declarations alphabetical sorting
-    'react/sort-prop-types': ,
+    'react/sort-prop-types': 'off',
 
     // Enforce style prop value being an object
-    'react/style-prop-object': ,
+    'react/style-prop-object': 'error',
 
     // Prevent void DOM elements(e.g. <img />, <br />) from receiving children
-    'react/void-dom-elements-no-children': ,
+    'react/void-dom-elements-no-children': 'error',
   },
 };
--- javascript/packages/eslint-config-monterail-react/rules/jsx.js
+++ javascript/packages/eslint-config-monterail-react/rules/jsx.js
@@ -1,87 +1,116 @@
 module.exports = {
   rules: {
     // Enforce boolean attributes notation in JSX(fixable)
-    'react/jsx-boolean-value': ,
+    'react/jsx-boolean-value': 'error',
 
     // Validate closing bracket location in JSX(fixable)
-    'react/jsx-closing-bracket-location': ,
+    'react/jsx-closing-bracket-location': ['error', 'line-aligned'],
 
     // Validate closing tag location in JSX(fixable)
-    'react/jsx-closing-tag-location': ,
+    'react/jsx-closing-tag-location': 'error',
 
     // Enforce or disallow spaces inside of curly braces in JSX attributes and expressions(fixable)
-    'react/jsx-curly-spacing': ,
+    'react/jsx-curly-spacing': ['error', 'never', {
+      'allowMultiline': true,
+    }],
 
     // Enforce or disallow spaces around equal signs in JSX attributes(fixable)
-    'react/jsx-equals-spacing': ,
+    'react/jsx-equals-spacing': ['error', 'never'],
 
     // Restrict file extensions that may contain JSX
-    'react/jsx-filename-extension': ,
+    'react/jsx-filename-extension': ['error', {
+        'extensions': [
+          '.js',
+          '.jsx',
+        ]
+      }
+    ],
 
     // Enforce position of the first prop in JSX(fixable)
-    'react/jsx-first-prop-new-line': ,
+    'react/jsx-first-prop-new-line': ['error', 'multiline'],
 
     // Enforce event handler naming conventions in JSX
-    'react/jsx-handler-names': ,
+    'react/jsx-handler-names': ['off', {
+      eventHandlerPrefix: 'handle',
+      eventHandlerPropPrefix: 'on',
+    }],
 
     // Validate JSX indentation(fixable)
-    'react/jsx-indent': ,
+    'react/jsx-indent': ['error', 2],
 
     // Validate props indentation in JSX(fixable)
-    'react/jsx-indent-props': ,
+    'react/jsx-indent-props': ['error', 2],
 
     // Validate JSX has key prop when in array or iterator
-    'react/jsx-key': ,
+    'react/jsx-key': 'error',
 
     // Limit maximum of props on a single line in JSX(fixable)
-    'react/jsx-max-props-per-line': ,
+    'react/jsx-max-props-per-line': ['error', 1],
 
     // Prevent usage of .bind() and arrow functions in JSX props
-    'react/jsx-no-bind': ,
+    'react/jsx-no-bind': ['error', {
+      ignoreRefs: true,
+      allowArrowFunctions: true,
+      allowFunctions: true,
+      allowBind: false,
+    }],
 
     // Prevent comments from being inserted as text nodes
-    'react/jsx-no-comment-textnodes': ,
+    'react/jsx-no-comment-textnodes': 'error',
 
     // Prevent duplicate props in JSX
-    'react/jsx-no-duplicate-props': ,
+    'react/jsx-no-duplicate-props': 'error',
 
     // Prevent usage of unwrapped JSX strings
-    'react/jsx-no-literals': ,
+    'react/jsx-no-literals': ['off'],
 
     // Prevent usage of unsafe target='_blank'
-    'react/jsx-no-target-blank': ,
+    'react/jsx-no-target-blank': 'error',
 
     // Disallow undeclared variables in JSX
-    'react/jsx-no-undef': ,
+    'react/jsx-no-undef': 'error',
 
     // Limit to one expression per line in JSX
-    'react/jsx-one-expression-per-line': ,
+    'react/jsx-one-expression-per-line': 'error',
 
     // Enforce curly braces or disallow unnecessary curly braces in JSX
-    'react/jsx-curly-brace-presence': ,
+    'react/jsx-curly-brace-presence': ['error', {
+      props: 'never',
+      children: 'never'
+    }],
 
     // Enforce PascalCase for user - defined JSX components
-    'react/jsx-pascal-case': ,
+    'react/jsx-pascal-case': 'error',
 
     // Enforce default props alphabetical sorting
-    'react/jsx-sort-default-props': ,
+    'react/jsx-sort-default-props': 'off',
 
     // Enforce props alphabetical sorting(fixable)
-    'react/jsx-sort-props': ,
+    'react/jsx-sort-props': 'off',
 
     // Validate spacing before closing bracket in JSX(fixable)
-    'react/jsx-space-before-closing': ,
+    'react/jsx-space-before-closing': ['error', 'always'],
 
     // Validate whitespace in and around the JSX opening and closing brackets(fixable)
-    'react/jsx-tag-spacing': ,
+    'react/jsx-tag-spacing': ['error', {
+      closingSlash: 'never',
+      beforeSelfClosing: 'always',
+      afterOpening: 'never',
+      beforeClosing: 'allow',
+    }],
 
     // Prevent React to be incorrectly marked as unused
-    'react/jsx-uses-react': ,
+    'react/jsx-uses-react': 'error',
 
     // Prevent variables used in JSX to be incorrectly marked as unused
-    'react/jsx-uses-vars': ,
+    'react/jsx-uses-vars': 'error',
 
     // Prevent missing parentheses around multilines JSX(fixable)
-    'react/jsx-wrap-multilines': ,
+    'react/jsx-wrap-multilines': ['error', {
+      declaration: true,
+      assignment: true,
+      return: true,
+      arrow: true,
+    }],
   },
 };
